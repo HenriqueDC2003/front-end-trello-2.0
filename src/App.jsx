8828e40f2b980684800b9768aaccc7351d29372d
HenriqueDCorrea <henriqueduartecorrea2005@gmail.com>
2024-12-05 07:18:35 -0300

    base do front toda junta

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import telaPrincipal from "./pages/tela-principal/src/App.jsx";
+
+
+function App() {
+  return (
+      <Router>
+          <Routes>
+              <Route path="/" element={<telaPrincipal />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+          </Routes>
+      </Router>
+  );
+}
+
+export default App
