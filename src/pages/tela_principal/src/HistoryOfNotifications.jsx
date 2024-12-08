f4e4414a6b7b2cd91202d4a97c06e742ec66208d
HenriqueDCorrea <henriqueduartecorrea2005@gmail.com>
2024-12-07 23:50:06 -0300

    troca do verde do recuperacao de senha

--- src/pages/tela_principal/src/HistoryOfNotifications.jsx
+++ src/pages/tela_principal/src/HistoryOfNotifications.jsx
@@ -5,7 +5,7 @@ const HistoryOfNotifications = () => {
     <div>
       <h1>Histórico de Notificações</h1>
       <p>Aqui estão suas notificações passadas...</p>
-      {/* Adicione o conteúdo do histórico de notificações aqui */}
+      {}
     </div>
   );
 };
