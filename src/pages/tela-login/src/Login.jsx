419775b568b5cceb5f6fa36bd078d83faa3f72d1
J00naTHan <jonathan.dutra.2004@gmail.com>
2024-12-12 17:50:00 -0300

    Login Update

--- src/pages/tela-login/src/Login.jsx
+++ src/pages/tela-login/src/Login.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import "./Login.css";
 
 const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate('/TelaPrincipal');  
+  // Estados para trabalhar com os dados
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Função para tratar o submit
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Evita o comportamento padrão de recarregar a página
+
+    // Efetuar fetch com a API para recuperar o usuário
+    const response = await fetch(`http://127.0.0.1:8000/api/users/get_by_username/${username}/`, {
+      method: 'GET',
+    });
+
+    if (!response.ok) {
+      alert('Não foi possível realizar login!');
+      return;
+    } else if (response.status === 404) {
+      alert(`O usuário de nome ${username} não foi encontrado!`);
+      return;
+    }
+
+    const user = await response.json();
+
+    navigate('/TelaPrincipal', { state: { user: user } });
   };
 
   const recupSenha = () => {
@@ -14,25 +36,37 @@ const Login = () => {
   };
 
   return (
-    <div className="login-container">
-      <div className="login-card">
-        <h2 className="login-title">Bem-vindo!</h2>
-        <form>
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Digite seu email" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Senha</label>
-            <input type="password" id="password" placeholder="Digite sua senha" />
-          </div>
-          <button type="submit" className="login-button" onClick={handleClick}>Entrar</button>
-        </form>
-        <p className="login-footer" onClick={recupSenha}>
-          Esqueceu sua senha? <a href="#">Recupere aqui</a>
-        </p>
+      <div className="login-container">
+        <div className="login-card">
+          <h2 className="login-title">Bem-vindo!</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="username">Nome de Usuário</label>
+              <input
+                  type="text"
+                  id="username"
+                  placeholder="Digite seu nome de usuário"
+                  value={username} // Valor vinculado ao estado
+                  onChange={(e) => setUsername(e.target.value)} // Atualiza o estado
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Senha</label>
+              <input
+                  type="password"
+                  id="password"
+                  placeholder="Digite sua senha"
+                  value={password} // Valor vinculado ao estado
+                  onChange={(e) => setPassword(e.target.value)} // Atualiza o estado
+              />
+            </div>
+            <button type="submit" className="login-button">Entrar</button>
+          </form>
+          <p className="login-footer" onClick={recupSenha}>
+            Esqueceu sua senha? <a href="#">Recupere aqui</a>
+          </p>
+        </div>
       </div>
-    </div>
   );
 };
 
